61e9a63fd7c3448017954f8c97d99788c409def5
Michael Filoramo <72812809+mfiloramo@users.noreply.github.com>
2024-05-21 12:45:58 -0400

    Debug webhook

--- src/controllers/orders.controller.ts
+++ src/controllers/orders.controller.ts
@@ -90,7 +90,7 @@ export const createOrder = async (req: Request, res: Response): Promise<void> =>
 
 export const updateOrderStatus = async (req: Request, res: Response): Promise<void> => {
   try {
-    console.log(req.body);
+    console.log(req.body.value.label);
 
     const { id, status, value } = req.body;
     console.log(value);
