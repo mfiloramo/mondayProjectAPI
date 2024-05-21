973e9767c18880231974e21f7ab5a4750d79c853
Michael Filoramo <72812809+mfiloramo@users.noreply.github.com>
2024-05-21 13:17:34 -0400

    Fix webhook route handling (event.type switch block)

--- src/controllers/orders.controller.ts
+++ src/controllers/orders.controller.ts
@@ -90,7 +90,7 @@ export const createOrder = async (req: Request, res: Response): Promise<void> =>
 
 export const updateOrderStatus = async (req: Request, res: Response): Promise<void> => {
   try {
-    console.log(req.body.value.label);
+    console.log(req.body.value);
 
     const { id, status, value } = req.body;
     console.log(value);
