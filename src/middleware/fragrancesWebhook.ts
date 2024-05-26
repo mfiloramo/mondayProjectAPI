aa22779d398b7964df56fe445df73633c8be336f
Michael Filoramo <72812809+mfiloramo@users.noreply.github.com>
2024-05-26 14:35:59 -0400

    Add webhook debugging

--- src/middleware/fragrancesWebhook.ts
+++ src/middleware/fragrancesWebhook.ts
@@ -3,6 +3,7 @@ import { addFragrance, updateFragrance, deleteFragrance } from '../controllers/f
 
 export const handleFragrancesWebhook = async (req: Request, res: Response): Promise<void> => {
   try {
+    console.log(req.body);
     // MONDAY.COM WEBHOOK VERIFIER
     if (req.body.challenge) {
       res.send({ challenge: req.body.challenge });
