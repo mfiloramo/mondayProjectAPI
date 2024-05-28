7783c83bcae2b01e2ca7eb3fed87d21bf559b719
Michael Filoramo <72812809+mfiloramo@users.noreply.github.com>
2024-05-28 13:45:53 -0400

    Add fragrance names to array and assign

--- src/controllers/orders.controller.ts
+++ src/controllers/orders.controller.ts
@@ -25,11 +25,10 @@ export const getAllOrders = async (req: Request, res: Response): Promise<void> =
 };
 
 export const createOrder = async (req: Request, res: Response): Promise<void> => {
-  // CREATE NEW ORDER
   try {
     const query: string = `
       query {
-        boards (ids: ${ process.env.BOARD_ID_ORDERS}) {
+        boards (ids: ${process.env.BOARD_ID_ORDERS}) {
           columns {
             id
             title
@@ -38,10 +37,9 @@ export const createOrder = async (req: Request, res: Response): Promise<void> =>
       }
     `;
     if (apiToken) {
-      const mondayResponse: AxiosResponse<any, any> = await mondayApiToken.post('', { query: query });
+      const mondayResponse: AxiosResponse<any, any> = await mondayApiToken.post('', { query });
       console.log("Monday API Response: ", mondayResponse.data.data.boards[0]);
     }
-
 
     const {
       first_name,
@@ -53,15 +51,23 @@ export const createOrder = async (req: Request, res: Response): Promise<void> =>
       status,
     } = req.body;
 
-    // ASSIGN INPUT VALUES FOR NEW ORDER
     const created_at: string = new Date().toISOString();
     const updated_at: string = new Date().toISOString();
     const numberOfKits: number = parseInt(number_of_kits, 10);
-    const fragrance1Id: bigint = fragrance1_id;
-    const fragrance2Id: bigint = fragrance2_id;
-    const fragrance3Id: bigint = fragrance3_id;
+    const fragranceIds: bigint[] = [fragrance1_id, fragrance2_id, fragrance3_id];
 
-    // LEVERAGE ORM TO SAVE NEW ORDER TO DATABASE
+    // Fetch fragrance names concurrently
+    const fragranceNames: any[] = await Promise.all(
+      fragranceIds.map(async (id: bigint): Promise<any> => {
+        const [result]: any = await sequelize.query('EXECUTE GetFragranceName :id', {
+          replacements: { id },
+        });
+        return result[0].name;
+      })
+    );
+
+    console.log("Fragrance Names: ", fragranceNames);
+
     const response: any = await sequelize.query(
       'EXECUTE CreateOrder :first_name, :last_name, :number_of_kits, :fragrance1_id, :fragrance2_id, :fragrance3_id',
       {
@@ -69,31 +75,17 @@ export const createOrder = async (req: Request, res: Response): Promise<void> =>
           first_name,
           last_name,
           number_of_kits: numberOfKits,
-          fragrance1_id: fragrance1Id,
-          fragrance2_id: fragrance2Id,
-          fragrance3_id: fragrance3Id,
-          created_at: created_at,
-          updated_at: updated_at
-        }
+          fragrance1_id: fragranceIds[0],
+          fragrance2_id: fragranceIds[1],
+          fragrance3_id: fragranceIds[2],
+          created_at,
+          updated_at,
+        },
       }
     );
 
-    // DATABASE OUTPUTS NEW ORDER ID
     const orderId: number = response[0][0].NewOrderID;
 
-    // GET FRAGRANCE NAMES
-    const fragranceIds: bigint[] = [ fragrance1Id, fragrance2Id, fragrance3Id ];
-    for (let fragranceId of fragranceIds) {
-      let id = fragranceId;
-      let fragranceName: any = await sequelize.query('EXECUTE GetFragranceName :id', {
-        replacements: { id }
-      })
-        .then((response: any): void => fragranceId = response);
-    }
-
-    console.log(fragranceIds);
-
-    // MUTATION QUERY STRING FOR MONDAY.COM API
     const mutation: string = `
       mutation {
         create_item (
@@ -104,11 +96,11 @@ export const createOrder = async (req: Request, res: Response): Promise<void> =>
             text__1: last_name,
             status7__1: status,
             quantity__1: numberOfKits,
-            fragrance_1_id6__1: fragrance1_id,
-            fragrance_2_id__1: fragrance2_id,
-            fragrance_3_id4__1: fragrance3_id,
+            fragrance_1_id6__1: fragranceNames[0],
+            fragrance_2_id__1: fragranceNames[1],
+            fragrance_3_id4__1: fragranceNames[2],
             text34__1: dayjs(created_at).format('MMMM D, YYYY'),
-            text4__1: dayjs(updated_at).format('MMMM D, YYYY')
+            text4__1: dayjs(updated_at).format('MMMM D, YYYY'),
           }).replace(/"/g, '\\"')}"
         ) {
           id
@@ -116,20 +108,17 @@ export const createOrder = async (req: Request, res: Response): Promise<void> =>
         }
       }`;
 
-    // VERIFY MONDAY.COM API TOKEN
     if (apiToken) {
       const mondayResponse: AxiosResponse<any, any> = await mondayApiToken.post('', { query: mutation });
       console.log("Success! Monday API Response: ", mondayResponse.data);
     }
 
-    // SEND NEW ORDER ID
     res.json({ orderId });
   } catch (error) {
     res.status(500).send(error);
     console.error(error);
   }
 };
-
 export const updateOrderStatus = async (req: Request, res: Response): Promise<void> => {
   // UPDATE ORDER STATUS
   try {
